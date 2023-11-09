5024ad7eb97ba593955cb818e8db18fd33289e91
Cody <codylau6233@hotmail.com>
2023-11-09 12:46:16 -0800

    Refactor fetcher.js: -node app make http request by using request function, fs.wroteFile function take data from request and write it to input localFilePath

--- fetcher.js
+++ fetcher.js
@@ -5,21 +5,29 @@ const arg = process.argv.slice(2);
 
 const url = arg[0];
 const localFilePath = arg[1];
+const encoding = 'utf8';
 
-const writeStream = fs.createReadStream(localFilePath);
+request(url, function(error, response, body) {
+  
+  if (error) {
+    console.error('error:', error); // Print the error if one occurred
+  }
+  if (response) {
+    console.log('statusCode:', response); // Print the response if a response was received
+    fs.writeFile(localFilePath, body, encoding, (error) => {
+      if (error) {
+        console.error(`Error writing to ${localFilePath}`, error);
+      } else {
+        console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`);
+      }
+    });
+  }
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+});
 
-request(url)
-  // this download data from the URL and save it to a local file
-  .pipe(writeStream)
-  // when the download is completed, on close, console.log message
-  .on('close', () =>{
-    console.log(`Downloaded and saved to ${localFilePath}`);
-  })
-  // console.error error, if there's any
-  .on('error', (error) => {
-    console.error('error', error);
-  });
-
+// handle error, if
+// node implementation to write to file fs.
+// implement through body
+// body.length
 
 // e.g message: Downloaded and saved 3261 bytes to ./index.html
-// .length of the data 1:1 is equal to 1 byte
